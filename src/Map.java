134ced6095a5cd99c0336b338f4e7e0a796be943
Simon <simonjonpedersen@gmail.com>
2015-02-02 14:00:29 +0100

    Updated Map.java

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
 
@@ -25,6 +26,9 @@ public class Map {
 
     /** Three pieces that make up the texture representing a monster. */
     public final String[] monsterTexture = { "|O-O|", ">-X-<", "_/ \\_" };
+
+    /** Three pieces that make up the texture representing a fight. */
+    public final String[] fightTexture = { "\\-|-/", "FIGHT", "/-|-\\" };
 
     /** This is the directory from where the map files is to be found. */
     private String mapDirectory;
@@ -103,6 +107,68 @@ public class Map {
         }
 
         return stringMap;
+
+    }
+
+    /**
+     * Return the first occurrence of the specified texture.
+     * @param texture - This is what kind of texture that are to be located.
+     *
+     */
+    public Point getTextureLocation(String[] texture) {
+
+        Point point = new Point();
+
+        for (int y = 0; y < map[0].length; y++)
+            for (int x = 0; x < map.length; x++)
+                if (map[x][y] == texture)
+                    point.setLocation(x, y);
+
+        return point;
+    }
+
+    /**
+     * Move the first occurrence of the specified texture to the specified location.
+     * This will only work if no wall is is in the way.
+     * A result in the form of string will also be returned.
+     * @param texture - This is what kind of texture that are to be moved.
+     * @param point - This is to where the texture should be moved.
+     *
+     */
+    public String moveTextureLocation(String[] texture, Point point) {
+
+        String result = "Failure: Wall";
+
+        Point previousPoint = getTextureLocation(texture);
+
+        if (map[point.x][point.y] == floorTexture) {
+
+            map[point.x][point.y] = texture;
+
+            map[previousPoint.x][previousPoint.y] = floorTexture;
+
+            result = "Success: Floor";
+
+        }
+        else if (texture == heroTexture && map[point.x][point.y] == monsterTexture) {
+
+            map[point.x][point.y] = fightTexture;
+
+            map[previousPoint.x][previousPoint.y] = floorTexture;
+
+            result = "Success: Monster";
+        }
+        else if (texture == monsterTexture && map[point.x][point.y] == heroTexture) {
+
+            map[point.x][point.y] = fightTexture;
+
+            map[previousPoint.x][previousPoint.y] = floorTexture;
+
+            result = "Success: Hero";
+
+        }
+
+        return result;
 
     }
 
