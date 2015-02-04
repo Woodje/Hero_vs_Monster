3a5ca75e65348c433ef160415be917ec90147a2d
krimanost <krimanost@hotmail.com>
2015-02-04 08:50:26 +0100

    The creation of the Hero and Monster class

---
+++ src/Hero.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kristoffer on 04-02-2015.
+ */
+public class Hero extends Character {
+
+    public Hero (String name, int skills) {
+
+        super(name, skills);
+
+    }
+
+}
---
+++ src/Monster.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Kristoffer on 04-02-2015.
+ */
+public class Monster extends Character {
+
+    public Monster(String name, int skills) {
+
+        super(name, skills);
+
+    }
+
+}
