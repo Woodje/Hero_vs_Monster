399baedd619fc95117bd6675f26d518599c2b5a7
krimanost <krimanost@hotmail.com>
2015-02-04 10:54:15 +0100

    Updated the Hero class

--- src/Hero.java
+++ src/Hero.java
@@ -7,14 +7,34 @@
  */
 public class Hero extends Character {
 
+    /** Represents the experience. */
+    private int experience;
+
     /**
      * Constructor.
-     * @param name - The name of the Hero.
+     * @param name - The name of the hero.
      * @param skills - Decides how many skills the monster has.
      */
     public Hero (String name, int skills) {
 
         super(name, skills);
+
+    }
+
+    /**
+     * Sets the experience.
+     * @param experience - The amount of experience.
+     */
+    public void setExperience(int experience) {
+
+        this.experience = experience;
+
+    }
+
+    /** Gets the experience */
+    public int getExperience() {
+
+      return experience;
 
     }
 
